d567bef860adff3626186a950e1ee625a83fd856
leadc <leandrodamian@hotmail.com>
2017-05-10 00:22:33 -0300

    Add files via upload

---
+++ TP-02/Entidades/Leche.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades_2017
+{
+    public class Leche : Producto
+    {
+        public enum ETipo { Entera, Descremada }
+        private ETipo _tipo;
+
+        /// <summary>
+        /// Por defecto, TIPO será ENTERA
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="patente"></param>
+        /// <param name="color"></param>
+        public Leche(EMarca marca, string patente, ConsoleColor color)
+            : base(patente, marca, color)
+        {
+            _tipo = ETipo.Entera;
+        }
+
+        public Leche(EMarca eMarca, string p, ConsoleColor consoleColor, ETipo eTipo):this(eMarca,p,consoleColor)
+        {
+            // TODO: Complete member initialization
+            
+            this._tipo = eTipo;
+        }
+
+        /// <summary>
+        /// Las leches tienen 20 calorías
+        /// </summary>
+        public override short CantidadCalorias
+        {
+            get
+            {
+                return 20;
+            }
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("LECHE");
+            sb.AppendLine((string)this);
+            sb.AppendLine("CALORIAS : " + this.CantidadCalorias.ToString());
+            sb.AppendLine("TIPO : " + this._tipo);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ TP-02/Entidades/Producto.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2017
+{
+    /// <summary>
+    /// La clase Producto será abstracta, evitando que se instancien elementos de este tipo.
+    /// </summary>
+    public class Producto
+    {
+        public enum EMarca
+        {
+            Serenisima, Campagnola, Arcor, Ilolay, Sancor, Pepsico
+        }
+        private EMarca _marca;
+        private string _codigoDeBarras;
+        //ConsoleColor _colorPrimarioEmpaque;
+        //private string patente;
+        private ConsoleColor _color;
+
+        public Producto(string patente, EMarca marca, ConsoleColor color)
+        {
+            // TODO: Complete member initialization
+            this._codigoDeBarras = patente;
+            this._marca = marca;
+            this._color = color;
+        }
+
+        /// <summary>
+        /// ReadOnly: Retornará la cantidad de ruedas del vehículo
+        /// </summary>
+        public virtual short CantidadCalorias { get; set; }
+
+        /// <summary>
+        /// Publica todos los datos del Producto.
+        /// </summary>
+        /// <returns></returns>
+        public virtual string Mostrar()
+        {
+            return (string)this;
+        }
+
+        public static explicit operator string(Producto p)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CODIGO DE BARRAS: " + p._codigoDeBarras);
+            sb.AppendLine("MARCA          : " + p._marca.ToString());
+            sb.AppendLine("COLOR EMPAQUE  : "+ p._color.ToString());
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Dos productos son iguales si comparten el mismo código de barras
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator ==(Producto v1, Producto v2)
+        {
+            return (v1._codigoDeBarras == v2._codigoDeBarras);
+        }
+        /// <summary>
+        /// Dos productos son distintos si su código de barras es distinto
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator !=(Producto v1, Producto v2)
+        {
+            return (v1._codigoDeBarras == v2._codigoDeBarras);
+        }
+    }
+}
